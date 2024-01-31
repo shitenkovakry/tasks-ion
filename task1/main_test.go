81050389db05b8bbfea8045b69e5fb6200b8e67d
kry shitenkova <shitenkovakris@mail.ru>
2024-01-31 19:45:20 +0200

    add some changes for tests of tasks1

--- task1/main_test.go
+++ task1/main_test.go
@@ -27,23 +27,23 @@ func Test_Min_Case1(t *testing.T) {
 func Test_Reasult_Case1(t *testing.T) {
 	array := []int{3, 5, 8, 6, 2, 7, 10, 11}
 
-	expectedMax := 11
 	expectedMin := 2
+	expectedMax := 11
 
-	actualMax, actualMin := GetResult(array)
+	actualMin, actualMax := GetResult(array)
 
-	assert.Equal(t, expectedMax, actualMax)
 	assert.Equal(t, expectedMin, actualMin)
+	assert.Equal(t, expectedMax, actualMax)
 }
 
 func Test_Reasult_Case2(t *testing.T) {
 	array := []int{}
 
-	expectedMax := 0
 	expectedMin := 0
+	expectedMax := 0
 
-	actualMax, actualMin := GetResult(array)
+	actualMin, actualMax := GetResult(array)
 
-	assert.Equal(t, expectedMax, actualMax)
 	assert.Equal(t, expectedMin, actualMin)
+	assert.Equal(t, expectedMax, actualMax)
 }
